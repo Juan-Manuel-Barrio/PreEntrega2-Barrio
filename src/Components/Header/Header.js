c647d831f4e4651c275b32be3ccb611b027b9aac
Juan Manuel <jmbarrio97@hotmail.com>
2023-07-29 02:49:47 -0300

    first commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../NavBar/NavBar.js";
+import NavBar from "../Navbar/NavBar";
 import logo_header from "../../logo_primario.png";
 
 const Header = ({ showAs }) => {
